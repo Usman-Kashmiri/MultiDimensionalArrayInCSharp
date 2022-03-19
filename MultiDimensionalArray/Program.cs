b10726090ebc2e74bb5fc7836f1e68a34a0f439f
Usman-Kashmiri <usmankashmiri378@gmail.com>
2022-03-19 08:34:16 +0500

    Jagged Multidimensional Arrays in C#

--- MultiDimensionalArray/Program.cs
+++ MultiDimensionalArray/Program.cs
@@ -10,29 +10,34 @@ namespace MultiDimensionalArray
     {
         static void Main(string[] args)
         {
-            //Rectangular Multidimensional Arrays
+            //Jagged Multidimensional Arrays
 
-            int[,] a = new int[3, 4] {
-               {0, 1, 2, 3} ,   //  initializers for row indexed by 0
-               {4, 5, 6, 7} ,   //  initializers for row indexed by 1
-               {8, 9, 10, 11}   //  initializers for row indexed by 2 
+            int[][] jagged_arr = new int[][]
+            {
+                new int[] {1, 2, 3, 4},
+                new int[] {11, 34, 67},
+                new int[] {89, 23},
+                new int[] {0, 45, 78, 53, 99}
             };
 
             Console.WriteLine("output using foreach loop");
             // output using foreach
-            foreach (var item in a)
+            foreach (var item in jagged_arr)
             {
-                Console.WriteLine(item);
+                foreach (var index in item)
+                {
+                    Console.WriteLine(index);
+                }
             }
 
             Console.WriteLine("output using nested for loop");
             /* output each array element's value */
             int i, j;
-            for (i = 0; i < a.GetLength(0); i++)
+            for (i = 0; i < jagged_arr.Length; i++)
             {
-                for (j = 0; j < a.GetLength(1); j++)
+                for (j = 0; j < jagged_arr[i].Length; j++)
                 {
-                    Console.WriteLine(a[i, j] + " ");
+                    Console.WriteLine(jagged_arr[i][j] + " ");
                 }
                 Console.WriteLine();
             }
